6230248b6aaee71b6bb58dac5eb8390e3623a3b9
murtinha <ericmurta@hotmail.com>
2020-01-16 14:40:52 -0300

    fix google login for android

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
  * @flow
  */
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   StyleSheet,
   View,
@@ -11,10 +11,38 @@ import {
   Button,
 } from 'react-native';
 import { GraphRequestManager, LoginManager, AccessToken, GraphRequest } from "react-native-fbsdk";
+import { GoogleSignin, GoogleSigninButton, statusCodes } from '@react-native-community/google-signin';
 import Profile from './profile';
 
+      // androidClientId: '982839903865-kvbb25c3plrgrngesqqos481v0l4qjhf.apps.googleusercontent.com',
 const App: () => React$Node = () => {
+  useEffect(() => {
+    GoogleSignin.configure();
+  }, []);
   const [state, setState] = useState(null);
+  const isSignedIn = async () => {
+    // const isSignedIn = await GoogleSignin.isSignedIn();
+    // const currentUser = await GoogleSignin.getCurrentUser();
+
+    await GoogleSignin.hasPlayServices();
+    await GoogleSignin.signIn()
+    .then(() => console.log('LOGADO')).catch(e => {
+      if (e.code === statusCodes.SIGN_IN_CANCELLED) {
+        // user cancelled the login flow
+        console.log('CANCELED', e)
+      } else if (e.code === statusCodes.IN_PROGRESS) {
+        console.log('PROGRESS', e)
+        // operation (e.g. sign in) is in progress already
+      } else if (e.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        console.log('NOT AVAILABLE', e)
+        // play services not available or outdated
+      } else {
+        console.log('AVAILABLE', e)
+        // some other error happened
+      }
+  });
+    // console.log('>>>>>>>',isSignedIn, currentUser, email)
+  };
   const onPress = () => {
 		LoginManager.logInWithPermissions(["public_profile"]).then(
       function(result) {
@@ -48,7 +76,17 @@ const App: () => React$Node = () => {
     <SafeAreaView style={styles.container}>
       {state 
         ? <Profile profile={state} />
-        : <Button title="Facebook" onPress={onPress} />
+        : (
+          <React.Fragment>
+            <Button title="Facebook" onPress={onPress} />
+            <GoogleSigninButton
+              style={{ width: 192, height: 48 }}
+              size={GoogleSigninButton.Size.Wide}
+              color={GoogleSigninButton.Color.Dark}
+              onPress={isSignedIn}
+            />
+          </React.Fragment>
+        )
       }
     </SafeAreaView>
   );
